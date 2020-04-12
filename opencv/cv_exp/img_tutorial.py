4286250cddbd2c852c5f819613763d465c48c562
michael-christen <mchristen96@gmail.com>
2020-04-12 12:58:02 -0700

    opencv messing around

--- opencv/cv_exp/img_tutorial.py
+++ opencv/cv_exp/img_tutorial.py
@@ -3,30 +3,30 @@
 import sys
 
 import numpy as np
-import cv2
+import cv2 as cv
 
 
 def get_image(path):
-    return cv2.imread(path, 0)
+    return cv.imread(path, cv.IMREAD_COLOR)
 
 
 def show_image(img, name):
-    cv2.imshow(name, img)
+    cv.imshow(name, img)
 
 
 def save_image(img, path):
-    cv2.imwrite(path, img)
+    cv.imwrite(path, img)
 
 
 def main(path):
-    img = cv2.imread(path, 0)
-    cv2.imshow('image',img)
-    k = cv2.waitKey(0)
-    if k == 27:         # wait for ESC key to exit
-        cv2.destroyAllWindows()
+    img = cv.imread(path, cv.IMREAD_COLOR)
+    cv.imshow('image',img)
+    k = cv.waitKey(0)
+    if k == 27 or k == ord('q'):  # wait for ESC key to exit
+        cv.destroyAllWindows()
     elif k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite(path + 'gray.jpg', img)
-        cv2.destroyAllWindows()
+        cv.imwrite(path + 'gray.jpg', img)
+        cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
--- opencv/cv_exp/video_tutorial.py
+++ opencv/cv_exp/video_tutorial.py
@@ -6,25 +6,28 @@ import cv2
 
 def main(load):
     cap = cv2.VideoCapture(load)
+    if not cap.isOpened():
+        print('Cannot open camera')
+        sys.exit()
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
     while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
         # write the flipped frame
         out.write(frame)
 
-	# Our operations on the frame come here
-	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Our operations on the frame come here
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-	    break
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
